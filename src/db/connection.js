e33d09cdb1e313f494d723c70423b4ad62afd52f
taylorh2002 <taylor.headford@resurgem.co.uk>
2021-03-30 16:46:55 +0100

    Set up mongodb

---
+++ src/db/connection.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+console.log(process.env.MONGO_URI);
+
+const connection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI { useNewUrlParser: true, useUnifiedTopology: true })
+        console.log("Successfully connected to mongodb")
+    } catch (error) {
+        console.log(error)
+    }
+}

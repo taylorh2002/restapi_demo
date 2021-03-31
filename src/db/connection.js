36881799528bebc0c8993fb0d3b2fd54227843c6
taylorh2002 <taylor.headford@resurgem.co.uk>
2021-03-31 10:43:37 +0100

    end of the day

--- src/db/connection.js
+++ src/db/connection.js
@@ -3,7 +3,7 @@ console.log(process.env.MONGO_URI);
 
 const connection = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI { useNewUrlParser: true, useUnifiedTopology: true })
+        await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
         console.log("Successfully connected to mongodb")
     } catch (error) {
         console.log(error)
--- src/server.js
+++ src/server.js
@@ -1,1 +1,35 @@
-console.log("working")
+require("./db/connection");
+const express = require("express")
+
+const port = process.env.PORT || 5000;
+
+const app = express ();    
+
+app.get("/user", async (req, res) => {
+    /* res.send({message: "API is working correctly"}) */
+
+    try {
+        const allUsers = await User.find({});
+        res.send(allUsers);
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+app.post("/user", async (req, res) => {
+    try {
+        const user = new User(req.body);
+        const returnedValue = await user.save();
+        res.send(`Successfully added ${returnedValue.name}`);
+    } catch (error) {
+        res.send(error);
+    }
+})
+
+app.post("/user", (req, res) => {
+    res.send({message: "Added user successfully"})
+})
+
+app.listen(5000, () => {
+    console.log(`Listening on port ${port}`)
+}); 
